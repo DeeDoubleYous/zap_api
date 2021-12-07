192f08492455f26084d0566115073ce601324e10
DeeDoubleYous <wishart.a470@gmail.com>
2021-12-07 14:55:32 +0000

    Change import of mysql to be specifically createConnection

--- src/tools/databaseLookup.ts
+++ src/tools/databaseLookup.ts
@@ -1,4 +1,4 @@
-import mysql from 'mysql2/promise';
+import { createConnection } from 'mysql2/promise';
 import { ICredentials } from '../interfaces';
 import { readFileSync } from 'fs';
 
@@ -9,7 +9,7 @@ const databaseLookUp = ():ICredentials => {
 const credentials: ICredentials = databaseLookUp();
 
 export const executeQuery = async (sql: string, params: any[]) => {
-    const connection = await mysql.createConnection(credentials);
+    const connection = await createConnection(credentials);
     const [results] = await connection.execute(sql, params);
     return results;
 };
